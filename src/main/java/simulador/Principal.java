c2bd3c9eb55b0c6d892f57eef14a0be1c3c537df
13rian Vargas <brianstevenvargasclavijo@gmail.com>
2024-05-07 22:01:55 -0500

    Interfaz de carga.
    Añado método wait() que permite contar segundos antes de iniciar la siguiente función.
    
    También añado la interfaz de carga de POKEMONDONGO y arreglo un poco los espacios del menú de pikachus.

--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -7,31 +7,167 @@ public class Principal {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("");
-        System.out.println(" » » » ¡Bienvenidos a PokeMondonGO! « « «");
+        System.out.println(" \n \n \n \n \n  "); //Espacio visual
 
-        System.out.println("   ___      _                               _               ___      \n" + //
-                        "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| | ___  _ __   / _ \\___  \n" + //
-                        " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |/ _ \\| '_ \\ / /_\\/ _ \\ \n" + //
-                        "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| | (_) | | | / /_\\\\ (_) |\n" + //
-                        "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\\___/|_| |_\\____/\\___/ \n" + //
+        System.out.println("Cargando...");
+
+        System.out.println(" \n \n \n \n \n  "); //Espacio visual
+        
+        wait(3);
+        
+        System.out.println("»»» Bienvenidos a PokeMondonGo «««");
+
+        System.out.println(" \n \n \n \n \n  "); //Espacio visual
+
+        wait(2);
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //P
+
+        System.out.println("   ___      \n" + //
+                           "  / _ \\\n" + //
+                           " / /_)/\n" + //
+                           "/ ___/\n" + //
+                       "\\/\n" + //
                         "                                                                     ");
 
-        System.out.println("");                        
+        System.out.println(" \n \n \n \n "); //Espacio visual //PO
+        
+        wait(1);
+
+        System.out.println("   ___      \n" + //
+                           "  / _ \\___ \n" + //
+                           " / /_)/ _ \\\n" + //
+                           "/ ___/ (_) |\n" + //
+                       "\\/    \\___/\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POK
+
+        wait(1);
+
+        System.out.println("   ___      _\n" + //
+                           "  / _ \\___ | | __\n" + //
+                           " / /_)/ _ \\| |/ /\n" + //
+                           "/ ___/ (_) |   <\n" + //
+                       "\\/    \\___/|_|\\_\\\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKE
+
+        wait(1);
+
+        System.out.println("   ___      _\n" + //
+                           "  / _ \\___ | | _____  \n" + //
+                           " / /_)/ _ \\| |/ / _ \\\n" + //
+                            "/ ___/ (_) |   <  __/\n" + //
+                       "\\/    \\___/|_|\\_\\___\\\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEM
+
+        wait(1);
+
+        System.out.println("   ___      _\n" + //
+                           "  / _ \\___ | | _____  /\\/\\\n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\\n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMO
+
+        wait(1);
+
+        System.out.println("   ___      _\n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___\n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\\n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) |\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\\___/\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMON
+
+        wait(1);
+
+        System.out.println("   ___      _\n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___  _ __\n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\\n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | |\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMOND
+
+        wait(1);
+
+        System.out.println("   ___      _                               _\n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| |\n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |\n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| |\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMONDO
+
+        wait(1);
+
+        System.out.println("   ___      _                               _\n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| | ___\n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |/ _ \\\n" + //
+                             "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| | (_) |\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\\___/\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMONDON
+
+        wait(1);
+
+        System.out.println("   ___      _                               _               \n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| | ___  _ __\n" + //
+                        " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |/ _ \\| '_ \\\n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| | (_) | | | |\n" + //
+                     "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\\___/|_| |_|\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMONDONG
+
+        wait(1);
+
+        System.out.println("   ___      _                               _               ___\n" + //
+                        "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| | ___  _ __   / _ \\\n" + //
+                    " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |/ _ \\| '_ \\ / /_\\\n" + //
+                        "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| | (_) | | | / /_\\\\\n" + //
+                  "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\\___/|_| |_\\____/\n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMONDONGO
+
+        wait(1);
+        
+        System.out.println("   ___      _                               _               ___      \n" + //
+                           "  / _ \\___ | | _____  /\\/\\   ___  _ __   __| | ___  _ __   / _ \\___  \n" + //
+                           " / /_)/ _ \\| |/ / _ \\/    \\ / _ \\| '_ \\ / _` |/ _ \\| '_ \\ / /_\\/ _ \\ \n" + //
+                           "/ ___/ (_) |   <  __/ /\\/\\ \\ (_) | | | | (_| | (_) | | | / /_\\\\ (_) |\n" + //
+                       "\\/    \\___/|_|\\_\\___\\/    \\/\\___/|_| |_|\\__,_|\\___/|_| |_\\____/\\___/ \n" + //
+                        "                                                                     ");
+
+        System.out.println(" \n \n \n \n "); //Espacio visual //POKEMONDONGO FINAL
+
+        wait(1);
        
-        System.out.println("       ,___          .-;'                           `;-.          ___,\n" + //
-                        "       `\"-.`\\_...._/`.` » » » Selecciona una opción:  `.`\\_...._/`.-\\\"`\n" + //
-                        "    ,      \\        /                                   \\        /      ,\n" + //
-                        " .-' ',    / ()   ()\\   (1) | Gestionar Entrenadores.   /()   () \\    .' `-.\n" + //
-                        "`'._   \\  /()    .  (|                                 |)   .   ()\\   /   _.'`\n" + //
-                        "    > .' ;,     -'-  /  (2) | Gestionar Pokémones.      \\  -'-    ,; '.  <\n" + //
-                        "   / <   |;,     __.;                                   ;.__     ,;|   >  \\\n" + //
-                        "   '-.'-.|  , \\    , \\  (3) | Iniciar Batalla.         / ,    / ,  |.-'.-'\n" + //
-                        "      `>.|;, \\_)    \\_)                               (_/    (_/ ,;|_.<`\n" + //
-                        "       `-;     ,    /   (4) | Salir.                    \\    ,     ;-`\n" + //
-                        "          \\    /   <                                     >   \\    /\n" + //
-                        "           '. <`'-,_)   » » » Enjoy                     (_,-'`> .'\n" + //
-                        "        b&n '._)                                             (_,'  b&n");
+        System.out.println("[Nico] ,___          .-;'                                    `;-.          ___, [Bri]\n" + //
+                        "       `\"-.`\\_...._/`.`   » » » Seleccione una opción: « « «   `.`\\_...._/`.-\"`\n" + //
+                        "    ,      \\        /                                            \\        /      ,\n" + //
+                        " .-' ',    / ()   ()\\      (1) | Gestionar Entrenadores.         /()   () \\    .' `-.\n" + //
+                        "`'._   \\  /()    .  (|                                          |)   .   ()\\   /   _.'`\n" + //
+                        "    > .' ;,     -'-  /     (2) | Gestionar Pokémones.            \\  -'-    ,; '.  <\n" + //
+                        "   / <   |;,     __.;                                            ;.__     ,;|   >  \\\n" + //
+                        "   '-.'-.|  , \\    , \\     (3) | Iniciar Batalla.               / ,    / ,  |.-'.-'\n" + //
+                        "      `>.|;, \\_)    \\_)                                        (_/    (_/ ,;|_.<`\n" + //
+                        "       `-;     ,    /      (4) | Salir.                          \\    ,     ;-`\n" + //
+                        "          \\    /   <                                              >   \\    /\n" + //
+                        "           '. <`'-,_)     » » »      PokéMondonGo      « « «      (_,-'`> .'\n" + //
+                        "            '._)                                                       (_,'");
 
         int option = sc.nextInt();
 
@@ -54,4 +190,13 @@ public class Principal {
                 break;
         }//cierra switch
     }//cierra main
+
+    public static void wait(int sec){
+        try {
+            Thread.sleep(sec * 500);
+         } catch (Exception e) {
+            System.out.println(e);
+         }
+    }//cierra wait
+    
 }//cierra class
