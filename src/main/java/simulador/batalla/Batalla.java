931082109bce031788696e674b25c6fcd7eedcc5
13rian Vargas <brians.vargasc@konradlorenz.edu.co>
2024-05-09 10:49:13 -0500

    Consejo de profe.
    Puse Batalla public y el ejemplo de if del profe

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -6,7 +6,7 @@ public class Batalla {
    private Pokemon pokemon1;
    private Pokemon pokemon2;
 
-   Batalla (Pokemon pokemon1, Pokemon pokemon2){
+   public Batalla (Pokemon pokemon1, Pokemon pokemon2){
       this.pokemon1 = pokemon1;
       this.pokemon2 = pokemon2;
    }//cierra constructor
@@ -14,8 +14,17 @@ public class Batalla {
    public Pokemon iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2){
       //esperar metodookemon.atacar
 
-   
       return pokemon1;
    }//cierra iniciarBatalla
+
+   /*
+      int tipoPokemon1 = 1;
+      int tipoPokemon2 = 2;
+
+        if(tipoPokemon1==TipoPokemon.BICHO && tipoPokemon2==TipoPokemon.DRAGON){
+            System.out.println("Gana el bicho");
+        } //ESTO LO HIZO EL PROFE XD
+   
+   */
 
 }//cierra class
