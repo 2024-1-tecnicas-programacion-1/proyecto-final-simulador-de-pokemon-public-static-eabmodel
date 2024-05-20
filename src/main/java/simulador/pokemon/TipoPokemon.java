372d73c8d888f1d99af21ec61d8175e4e3d65a01
niko <nicolasgarciaperez34@gmail.com>
2024-05-20 10:27:15 -0500

    TipoPokemon
    Multiplicador de daño

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,21 +1,12 @@
 package simulador.pokemon;
 
-public class TipoPokemon{
-    public static final int BICHO = 1;
-    public static final int DRAGON = 2;
-    public static final int ELECTRICO = 3;
-    public static final int HADA = 4;
-    public static final int LUCHA = 5;
-    public static final int FUEGO = 6;
-    public static final int VOLADOR = 7;
-    public static final int FANTASMA = 8;
-    public static final int PLANTA = 9;
-    public static final int TIERRA = 10;
-    public static final int HIELO = 11;
-    public static final int NORMAL = 12;
-    public static final int VENENO = 13;
-    public static final int PSIQUICO = 14;
-    public static final int ROCA = 15;
-    public static final int ACERO = 16;
-    public static final int AGUA = 17;
+public enum TipoPokemon{
+    FUEGO, AGUA, BICHO, ELECTRICO, PSIQUICO, TIERRA, HIELO, VOLADOR, NORMAL, LUCHA, VENENO
+
+
+public static double obtenerMultiplicador(TipoPokemon atacante, TipoPokemon defensor){
+
+
+
+}
 }//cierra class
