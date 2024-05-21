bea29c55c91f8cbb714044f98f37e800c413e27f
13rian Vargas <brians.vargasc@konradlorenz.edu.co>
2024-05-20 21:17:21 -0500

    Tipo Pokemon Update
    Añadí un TODO, también saltos de linea y comentarios en los cierre de corchetes.

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -3,176 +3,233 @@ package simulador.pokemon;
 public enum TipoPokemon{
     FUEGO, AGUA, PLANTA, VENENO, ELECTRICO, PSIQUICO, ROCA, TIERRA, NORMAL, VOLADOR, HADA, LUCHA, ACERO, BICHO, HIELO, FANTASMA; 
 
-public static double obtenerMultiplicador(TipoPokemon atacante, TipoPokemon defensor){
-switch (atacante) {
-    case FUEGO:
-        switch (defensor) {
-            case PLANTA:
-            case BICHO:    
-            case HIELO:
-            return 2.0;
+    public static double obtenerMultiplicador(TipoPokemon atacante, TipoPokemon defensor){
+        switch (atacante) {
+
+            case FUEGO:
+                switch (defensor) {
+
+                    case PLANTA:
+                    case BICHO:    
+                    case HIELO:
+                    return 2.0;
+
+                    case AGUA:
+                    case ROCA:
+                    case ACERO:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
             case AGUA:
-            case ROCA:
-            case ACERO:
-            return 0.5;   
-        }
-        break;    
-    case AGUA:
-    switch (defensor){
-            case FUEGO:
-            case ROCA:
-            return 2.0;
+                switch (defensor){
+
+                    case FUEGO:
+                    case ROCA:
+                    return 2.0;
+
+                    case PLANTA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
             case PLANTA:
-            return 0.5;
+                switch(defensor){
 
-        }
-        break;
-    case PLANTA:
-    switch(defensor){
-            case AGUA:
-            case ROCA:
-            case TIERRA:
-            return 2.0;
-            case FUEGO:
+                    case AGUA:
+                    case ROCA:
+                    case TIERRA:
+                    return 2.0;
+
+                    case FUEGO:
+                    case VENENO:
+                    case BICHO:
+                    case ACERO:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
             case VENENO:
-            case BICHO:
-            case ACERO:
-            return 0.5;
+                switch(defensor){
 
-        }
-        break;
-    case VENENO:
-    switch(defensor){
-            case PLANTA:
-            case HADA:
-            return 2.0;
-            case ROCA:
-            case FANTASMA:
-            return 0.5;
+                    case PLANTA:
+                    case HADA:
+                    return 2.0;
 
-        }
-        break;    
-    case ELECTRICO:
-    switch (defensor){
-            case AGUA:
-            case VOLADOR:
-            return 2.0;
-            case PLANTA:
-            return 0.5;
+                    case ROCA:
+                    case FANTASMA:
+                    return 0.5;
 
-        }
-        break;   
-    case PSIQUICO:
-    switch(defensor){
-            case VENENO:
-            case LUCHA:
-            return 2.0;
-        
-        }     
-        break;
-    case ROCA:
-    switch(defensor){
-            case FUEGO:
-            case VOLADOR:
-            case ACERO:
-            case BICHO:
-            case HIELO:
-            return 2.0;
-            case LUCHA:
-            return 0.5;
-        }
-        break;
-    case TIERRA:
-    switch(defensor){
-            case FUEGO:
-            case VENENO:
-            case ROCA:
-            case ACERO:
-            return 2.0;
+                }//cierra switch
+                break;
+
             case ELECTRICO:
-            return 0.0;
-            case VOLADOR:
-            return 0.5;
-        }
-        break;
-    case NORMAL:
-    switch(defensor){
-            case FANTASMA:
-            return 0.0;
-        }
-        break;
-    case VOLADOR:
-    switch (defensor){
-            case PLANTA:
-            case ELECTRICO:
-            case BICHO:
-            return 2.0;
-            case ROCA:
-            return 0.5;
-        }
-        break;
-    case HADA:
-    switch (defensor){
-              case PSIQUICO:
-              return 2.0;      
-        }
-        break;
-    case LUCHA:
-    switch (defensor){
-            case ROCA:
-            case NORMAL:
-            case HIELO:
-            return 2.0;
-            case PSIQUICO:
-            case HADA:
-            case FANTASMA:
-            return 0.5;
-        } 
-        break;     
-    case ACERO:
-    switch(defensor){
-            case FUEGO:
-            case ROCA:
-            case HADA:
-            case LUCHA:
-            return 2.0;
-            case PSIQUICO:
-            return 0.5;
-        }
-        break;    
-    case BICHO:
-    switch(defensor){
-            case PLANTA:
-            case VOLADOR:
-            return 2.0;
-            case ACERO:
-            case FANTASMA:
-            return 0.5;
+                switch (defensor){
+                    
+                    case AGUA:
+                    case VOLADOR:
+                    return 2.0;
 
-        }
-        break;
-    case HIELO:
-    switch(defensor){
-            case PLANTA:
+                    case PLANTA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            case PSIQUICO:
+                switch(defensor){
+
+                    case VENENO:
+                    case LUCHA:
+                    return 2.0;
+                
+                }//cierra switch     
+                break;
+
+            case ROCA:
+                switch(defensor){
+
+                    case FUEGO:
+                    case VOLADOR:
+                    case ACERO:
+                    case BICHO:
+                    case HIELO:
+                    return 2.0;
+
+                    case LUCHA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
             case TIERRA:
-            return 2.0;
-        }
-        break;
-    case FANTASMA:
-    switch(defensor){
-            case FANTASMA:
-            return 2.0;
-            case NORMAL:
-            return 0.0;
-            case VENENO:
-            return 0.5;
-        }
-        break;
+                switch(defensor){
 
-        default:
-        }
+                    case FUEGO:
+                    case VENENO:
+                    case ROCA:
+                    case ACERO:
+                    return 2.0;
+
+                    case ELECTRICO:
+                    return 0.0;
+                    
+                    case VOLADOR:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            case NORMAL:
+                switch(defensor){
+
+                    case FANTASMA:
+                    return 0.0;
+
+                }//cierra switch
+                break;
+
+            case VOLADOR:
+                switch (defensor){
+
+                    case PLANTA:
+                    case ELECTRICO:
+                    case BICHO:
+                    return 2.0;
+
+                    case ROCA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            case HADA:
+                switch (defensor){
+
+                    case PSIQUICO:
+                    return 2.0;   
+
+                }//cierra switch
+                break;
+
+            case LUCHA:
+                switch (defensor){
+
+                    case ROCA:
+                    case NORMAL:
+                    case HIELO:
+                    return 2.0;
+
+                    case PSIQUICO:
+                    case HADA:
+                    case FANTASMA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            case ACERO:
+                switch(defensor){
+
+                    case FUEGO:
+                    case ROCA:
+                    case HADA:
+                    case LUCHA:
+                    return 2.0;
+
+                    case PSIQUICO:
+                    return 0.5;
+
+                }//cierra switch
+                break;    
+            case BICHO:
+                switch(defensor){
+
+                    case PLANTA:
+                    case VOLADOR:
+                    return 2.0;
+
+                    case ACERO:
+                    case FANTASMA:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            case HIELO:
+                switch(defensor){
+
+                    case PLANTA:
+                    case TIERRA:
+                    return 2.0;
+
+                }//cierra switch
+                break;
+
+            case FANTASMA:
+                switch(defensor){
+
+                    case FANTASMA:
+                    return 2.0;
+
+                    case NORMAL:
+                    return 0.0;
+
+                    case VENENO:
+                    return 0.5;
+
+                }//cierra switch
+                break;
+
+            default: //TODO @Niko esto que onda? xd
+                
+        }//cierra switch general
+
         return 1.0;
-    }
-       
-}//cierra class
+
+    }//cierra obtenerMultiplicador
+}//cierra enum
 
