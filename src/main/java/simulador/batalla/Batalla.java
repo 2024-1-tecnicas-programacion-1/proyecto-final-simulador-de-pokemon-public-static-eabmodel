8d9151774dccb5dd15682192b7f8b1ef453024eb
Niko2817 <nicolasgarciaperez34@gmail.com>
2024-05-23 12:11:09 -0500

    Final Update Batalla.

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -4,32 +4,27 @@ import simulador.pokemon.Pokemon;
 
 public class Batalla {
    
-   public Pokemon iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2){
+   public String iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2){
       
+      String ganador;
+
       while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+         
          pokemon1.atacar(pokemon2);
+
          if (pokemon2.getSalud() > 0 ){
             pokemon2.atacar(pokemon1);
-         }
-      }
-   if (pokemon1.getSalud() <= 0) {
-      System.out.println("El ganador es: " + pokemon2.getNombre());
-   } else {
-      System.out.println("El ganador es: " + pokemon1.getNombre());
-   }
-      //esperar metodookemon.atacar
+         }//cierra if
 
-      return pokemon1;
+      }//cierra while
+      
+      if (pokemon1.getSalud() <= 0) {
+         ganador = pokemon2.getNombre();
+      } else {
+         ganador = pokemon2.getNombre();
+      }//cierra if
+
+      return ganador;
    }//cierra iniciarBatalla
 
-   /*
-      int tipoPokemon1 = 1;
-      int tipoPokemon2 = 2;
-
-        if(tipoPokemon1==TipoPokemon.BICHO && tipoPokemon2==TipoPokemon.DRAGON){
-            System.out.println("Gana el bicho");
-        } //ESTO LO HIZO EL PROFE XD
-   
-   */
-
-}//cierra class
+}//cierra class
