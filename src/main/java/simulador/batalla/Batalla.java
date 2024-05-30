c00b784fcc872568c8d14636f8e43810b95c0242
13rian Vargas <brians.vargasc@konradlorenz.edu.co>
2024-05-30 03:15:53 -0500

    Batalla Update
    - implementación método iniciar batalla.

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,31 +1,33 @@
 package simulador.batalla;
 
+import simulador.Principal;
 import simulador.pokemon.Pokemon;
 
 public class Batalla {
    
-   public static String iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2){
+   public static String iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2, boolean verif, String nombrePokemonGanador){
       
-      String nombrePokemonGanador;
+      while (verif == true){
+         while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+            
+            Principal.subMenuDuranteLaBatalla();
 
-      while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+            if (pokemon2.getSalud() > 0 ){
+               Principal.subMenuDuranteLaBatalla();
+               pokemon2.atacar(pokemon1);
+            }//cierra if
+
+         }//cierra while
          
-         pokemon1.atacar(pokemon2);
-
-         if (pokemon2.getSalud() > 0 ){
-            pokemon2.atacar(pokemon1);
-         }//cierra if
-
+         if (pokemon1.getSalud() < 0) {
+            nombrePokemonGanador = pokemon2.getNombre();
+         } else if (pokemon2.getSalud() < 0) {
+            nombrePokemonGanador = pokemon1.getNombre();
+         } else {
+            nombrePokemonGanador = "Error de ganador.";
+         }//cierra else-if
+         
       }//cierra while
-      
-      if (pokemon1.getSalud() > 0) {
-         nombrePokemonGanador = pokemon1.getNombre();
-      } else if (pokemon2.getSalud() > 0) {
-         nombrePokemonGanador = pokemon2.getNombre();
-      } else {
-         nombrePokemonGanador = "Error de ganador.";
-      }//cierra else-if
-
       return nombrePokemonGanador;
       
    }//cierra iniciarBatalla
